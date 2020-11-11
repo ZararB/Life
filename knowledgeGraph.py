e078d71efd47d0e3ee69a765cab4359abdb60e4f
ZararB <zararb99@gmail.com>
2020-11-10 22:08:18 -0500

    Create knowledgeGraph.py

---
+++ knowledgeGraph.py
@@ -0,0 +1,16 @@
+class KnowledgeGraph:
+
+    def __init__(self, root):
+    
+        self.root = root
+        self.
+        
+        
+        
+class Node:
+
+    def __init__(self, name, description=None, parent=None):
+
+        self.name = name
+        self.description = description or []
+        self.parent = parent 
